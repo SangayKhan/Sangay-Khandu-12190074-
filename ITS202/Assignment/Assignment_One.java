4cf118a1849fcd0eeb9b81662a24ae38d8be4d29
SangayKhan <66408515+SangayKhan@users.noreply.github.com>
2020-09-30 20:20:50 +0600

    edit code

--- ITS202/Assignment/Assignment_One.java
+++ ITS202/Assignment/Assignment_One.java
@@ -1,12 +1,20 @@
 import java.lang.Math;
-public class ArrayListTest{
+public class LinkedListTest{
 	public static void main(String[] args){
-		int initial = 4;
+		int initial = 10;
 		ArrayList obj = new ArrayList(initial);
 
 		//adding element to the array
-		obj.add(1);
-	
+		//need to warn the program if the add() is called more than the size 
+		//if(obj.count == obj.size){
+			System.out.println("All space full in array");
+		//}else{
+			obj.add(1);
+			obj.add(1);
+
+		//}
+
+
 		//element in the array before shrinking
 		System.out.println("Elements in array before:");
 
@@ -48,21 +56,21 @@ public class ArrayListTest{
 				System.out.print(obj.array[i] + " "); 
 			}
 
-			System.out.println("\nsize of the has changed as array is full by 1/4 or 3/4 is "+obj.size);
+			System.out.println("\nsize of the array has changed as array is full by 1/4 or 3/4 is "+obj.size);
 		
 		}else{
 
 			for (int i = 0; i < obj.size; i++) { 
 				System.out.print(obj.array[i] + " "); 
 			}
-			System.out.println("\nsize of the element remains same that is "+obj.size);
+			System.out.println("\nsize of the array remains same that is "+obj.size);
 		}
 		System.out.println("Number of element in array after is "+obj.count);
 	}
 }
 
 class ArrayList{
-	int size,initial_size;//,constant;
+	int size;//initial_size;//,constant;
 	int count = 0;
 	int full_1over4;
 	int full_3over4;
@@ -78,20 +86,8 @@ class ArrayList{
 	//will add new element in the array
 	public void add(int add_ele)
 	{
-		if(count!=size){//count should be less than size
-			array[count] = add_ele;
-		}else{//if the count is equal to number of size increase size by 1
-
-			int[] temp = new int[size+1];
-
-			for(int i=0;i<size;i++){
-				temp[i]=array[i];
-			}
-
-			array = temp;
-			array[count] = add_ele;
-		}
-		count++;	
+		array[count] = add_ele;
+		count++;
 	}
 	//remove the last element
 	public void pop()
@@ -108,7 +104,7 @@ class ArrayList{
 	//resize the size of array to double if array is full by 75% and half if 25% full
 	public void resize()
 	{
-		if(initial_size == full_1over4){
+		if(count == full_1over4){
 
 			int[] temp = new int[size/2];
 
@@ -120,7 +116,7 @@ class ArrayList{
 			array = temp;
 		}
 
-		if(initial_size == full_3over4){
+		if(count == full_3over4){
 
 			int[] temp = new int[size*2];
 
@@ -135,39 +131,12 @@ class ArrayList{
 
 	public int check()
 	{
-		shrinkSize();
-		return initial_size;
+		return count;
 	}
 
 	public String toString()
 	{
 		String s=Integer.toString(size);
 		return s;
-	}
-	//make count and size same
-	public void shrinkSize() 
-    { 
-    	int temp[] = null;
-
-       	if (count > 0){ 
-
-           	temp = new int[count];
-
-           	for (int i = 0; i < count; i++) {  
-               	temp[i] = array[i]; 
-           	}
-
-          	initial_size = count; 
-
-          	if(initial_size == full_1over4 || initial_size == full_3over4){
-           		array = temp;
-           	}else{
-           		temp = new int[size];
-           		for(int i=0;i<size;i++) {
-           			temp[i]=array[i];
-           		}
-           		array = temp;
-    		}
-        }
-    } 
+	} 
 }
