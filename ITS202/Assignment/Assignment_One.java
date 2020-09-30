bd5f6868bee1e36dd472c05b70082e214b79d7cd
SangayKhan <66408515+SangayKhan@users.noreply.github.com>
2020-09-30 12:54:53 +0600

    edit pop and shrinlsize method

--- ITS202/Assignment/Assignment_One.java
+++ ITS202/Assignment/Assignment_One.java
@@ -6,45 +6,63 @@ public class ArrayListTest{
 
 		//adding element to the array
 		obj.add(1);
-
-		
+	
 		//element in the array before shrinking
-		System.out.println("Elements of before array:"); 
+		System.out.println("Elements in array before:");
+
 		for (int i = 0; i < obj.size; i++) { 
-			System.out.println(obj.array[i] + " "); 
+			System.out.print(obj.array[i] + " "); 
 		}
 
 		//size of the element before shrinking
-		System.out.println("size of the array before is "+obj.size);
+		System.out.println("\nsize of the array before is "+obj.size);
 		System.out.println("Number of element in array bofore is "+obj.count);
 
-		int constant = obj.size();
-		obj.constant(constant);
+		//removing the last element of the array
+		obj.pop();
+
+		//element in the array after removing last element
+		System.out.println("\nElements in array after pop:");
+
+		for (int i = 0; i < obj.size; i++) { 
+			System.out.print(obj.array[i] + " "); 
+		}
+
+		//size of the element after removing last element
+		System.out.println("\nsize of the array after pop "+obj.size);
+		System.out.println("Number of element in array after pop is "+obj.count);
+
+		//make size as that of number of element in array 
+		obj.check();
+		//obj.constant(constant);
 		obj.resize();
 
 		
 
 		//size of the element after shrinking
+		System.out.println("\nElements in array after:");
+
 		if(obj.size!=initial){
-			//element in the array after shrinking
-			System.out.println("Elements of after array:"); 
+
 			for (int i = 0; i < obj.size; i++) { 
-				System.out.println(obj.array[i] + " "); 
+				System.out.print(obj.array[i] + " "); 
 			}
-			System.out.println("size of the has changed as array is full by 1/4 or 3/4 is "+obj.size);
+
+			System.out.println("\nsize of the has changed as array is full by 1/4 or 3/4 is "+obj.size);
+		
 		}else{
-			System.out.println("Elements of after array:"); 
+
 			for (int i = 0; i < obj.size; i++) { 
-				System.out.println(obj.array[i] + " "); 
+				System.out.print(obj.array[i] + " "); 
 			}
-			System.out.println("size of the element remains same that is "+obj.size);
+			System.out.println("\nsize of the element remains same that is "+obj.size);
 		}
 		System.out.println("Number of element in array after is "+obj.count);
 	}
 }
 
 class ArrayList{
-	int size,initial_size,constant;
+	int size,initial_size;//,constant;
 	int count = 0;
 	int full_1over4;
 	int full_3over4;
@@ -61,11 +79,14 @@ class ArrayList{
 	{
 		if(count!=size){
 			array[count] = add_ele;
-		}else{	
+		}else{
+
 			int[] temp = new int[size+1];
+
 			for(int i=0;i<size;i++){
 				temp[i]=array[i];
 			}
+
 			array = temp;
 			array[count] = add_ele;
 		}
@@ -76,38 +97,43 @@ class ArrayList{
 	{
 		if(count > 0){
 			// shrinkSize();
-            array[count] = 0; 
+            array[count-1] = 0; 
             count--; 
         } 	
 	}
 
-	public int constant(int constant){
-		this.constant = constant;
-		return constant;
+	public int size(){
+		return size;
 	}
 
 	public void resize()
 	{
-		if(constant == full_1over4){
+		if(initial_size == full_1over4){
+
 			int[] temp = new int[size/2];
+
 			for(int i=0;i<count;i++){
 				temp[i]=array[i];
 			}
+
 			size = size/2;
 			array = temp;
 		}
 
-		if(constant == full_3over4){
+		if(initial_size == full_3over4){
+
 			int[] temp = new int[size*2];
+
 			for(int i=0;i<count;i++){
 				temp[i]=array[i];
 			}
+
 			size = size*2;
 			array = temp;
 		}	
 	}
 
-	public int size()
+	public int check()
 	{
 		shrinkSize();
 		return initial_size;
@@ -121,16 +147,27 @@ class ArrayList{
 
 	public void shrinkSize() 
     { 
-    	//if(constant == full_1over4 || constant == full_3over4){
-    		int temp[] = null; 
-        	if (count > 0) { 
-            	temp = new int[count]; 
-            	for (int i = 0; i < count; i++) {  
-                	temp[i] = array[i]; 
-            	} 
-            	initial_size = count; 
-            	array = temp; 
+    	int temp[] = null;
+
+       	if (count > 0){ 
+
+           	temp = new int[count];
+
+           	for (int i = 0; i < count; i++) {  
+               	temp[i] = array[i]; 
+           	}
+
+          	initial_size = count; 
+
+          	if(initial_size == full_1over4 || initial_size == full_3over4){
+           		array = temp;
+           	}else{
+           		temp = new int[size];
+           		for(int i=0;i<size;i++) {
+           			temp[i]=array[i];
+           		}
+           		array = temp;
     		}
-       // } 
+        }
     } 
 }
