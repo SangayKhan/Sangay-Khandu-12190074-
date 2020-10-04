8611f49682aa00f7ae6d2a31fd36837d040dfef6
SangayKhan <66408515+SangayKhan@users.noreply.github.com>
2020-10-04 23:24:32 +0600

    uploding assignment

--- ITS202/Assignment/Assignment_One.java
+++ ITS202/Assignment/Assignment_One.java
@@ -1,90 +1,21 @@
 import java.lang.Math;
-public class LinkedListTest{
-	public static void main(String[] args){
-		int initial = 10;
-		ArrayList obj = new ArrayList(initial);
-
-		//adding element to the array
-		//need to warn the program if the add() is called more than the size 
-		//if(obj.count == obj.size){
-			System.out.println("All space full in array");
-		//}else{
-			obj.add(1);
-			obj.add(1);
-
-		//}
-
-
-		//element in the array before shrinking
-		System.out.println("Elements in array before:");
-
-		for (int i = 0; i < obj.size; i++) { 
-			System.out.print(obj.array[i] + " "); 
-		}
-
-		//size of the element before shrinking
-		System.out.println("\nsize of the array before is "+obj.size);
-		System.out.println("Number of element in array bofore is "+obj.count);
-
-		//removing the last element of the array
-		obj.pop();
-
-		//element in the array after removing last element
-		System.out.println("\nElements in array after pop:");
-
-		for (int i = 0; i < obj.size; i++) { 
-			System.out.print(obj.array[i] + " "); 
-		}
-
-		//size of the element after removing last element
-		System.out.println("\nsize of the array after pop "+obj.size);
-		System.out.println("Number of element in array after pop is "+obj.count);
-
-		//make size as that of number of element in array 
-		obj.check();
-		//obj.constant(constant);
-		obj.resize();
-
-		
-
-		//size of the element after shrinking
-		System.out.println("\nElements in array after:");
-
-		if(obj.size!=initial){
-
-			for (int i = 0; i < obj.size; i++) { 
-				System.out.print(obj.array[i] + " "); 
-			}
-
-			System.out.println("\nsize of the array has changed as array is full by 1/4 or 3/4 is "+obj.size);
-		
-		}else{
-
-			for (int i = 0; i < obj.size; i++) { 
-				System.out.print(obj.array[i] + " "); 
-			}
-			System.out.println("\nsize of the array remains same that is "+obj.size);
-		}
-		System.out.println("Number of element in array after is "+obj.count);
-	}
-}
-
-class ArrayList{
-	int size;//initial_size;//,constant;
+import java.util.Arrays;
+public class ArrayList<T>{
+	int size;
 	int count = 0;
 	int full_1over4;
 	int full_3over4;
-	int[] array;
+	Object[] array;
 
 	public ArrayList(int length){
 		size = length;
-		array = new int[size];
+		array = new Object[size];
 		full_1over4 = Math.round((25*length)/100);
 		full_3over4 = Math.round((75*length)/100);
 	}
 
 	//will add new element in the array
-	public void add(int add_ele)
+	public void add(T add_ele, Object[] array)
 	{
 		array[count] = add_ele;
 		count++;
@@ -97,7 +28,7 @@ class ArrayList{
             count--; 
         } 	
 	}
-
+	//return the size of the array
 	public int size(){
 		return size;
 	}
@@ -106,7 +37,7 @@ class ArrayList{
 	{
 		if(count == full_1over4){
 
-			int[] temp = new int[size/2];
+			Object[] temp = new Object[size/2];
 
 			for(int i=0;i<count;i++){
 				temp[i]=array[i];
@@ -118,7 +49,7 @@ class ArrayList{
 
 		if(count == full_3over4){
 
-			int[] temp = new int[size*2];
+			Object[] temp = new Object[size*2];
 
 			for(int i=0;i<count;i++){
 				temp[i]=array[i];
@@ -128,15 +59,24 @@ class ArrayList{
 			array = temp;
 		}	
 	}
-
-	public int check()
-	{
-		return count;
-	}
-
+	//retunr size of the array in staing type
 	public String toString()
 	{
-		String s=Integer.toString(size);
-		return s;
-	} 
+		String temp = Arrays.toString(array);
+		return temp; 
+	}
+
+	public static void main(String[] args){
+		ArrayList<Integer> obj = new ArrayList<>(10);
+
+		//adding element to the array
+		obj.add(1, obj.array);
+		obj.add(2, obj.array);
+		obj.add(3, obj.array);
+		obj.add(4, obj.array);
+
+		assert(obj.size()==10);
+		assert(obj.toString().equals("[1, 2, 3, 4, null, null, null, null, null, null]"));
+		System.out.println("All test pass. ");
+	}
 }
