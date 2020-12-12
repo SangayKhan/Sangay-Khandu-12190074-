a5d9831776b4006bed880d314cfff67ccbeb6639
SangayKhan <66408515+SangayKhan@users.noreply.github.com>
2020-12-12 21:13:32 +0600

    Update and rename lab10.java to lab9.java

--- ITS202/lab/lab9/Directed/lab9.java
+++ ITS202/lab/lab9/Directed/lab9.java
@@ -1,12 +1,12 @@
 import java.util.NoSuchElementException;
 
-public class lab10 {
+public class lab1 {
     private final int V;                  // number of vertices in this digraph
     private int E;                        // number of edges in this digraph
     private LinkedList<Integer>[] adj;    // adj[v] = adjacency list for vertex v
     private int[] indegree;               // indegree[v] = indegree of vertex v
     
-    public lab10(int V) {
+    public lab1(int V) {
     	this.V = V;
     	this.E = 0;
 
@@ -40,7 +40,7 @@ public class lab10 {
         return adj[v].keys();
     }
    
-/*    public lab10 reverse() {
+/*    public lab1 reverse() {
         
     }
     
@@ -50,7 +50,7 @@ public class lab10 {
 
     public static void main(String[] args) {
 
-        lab10 obj = new lab10(5);
+        lab1 obj = new lab1(5);
         obj.addEdge(1,3);
         obj.addEdge(1,4);
         obj.addEdge(1,2);
